b05a75a1102cfd888c6765a631a0f1400c3ef1a3
stoyan <stoyan@coherent-labs.com>
2013-01-15 13:00:38 +0200

    * DumpReferences is an add-in for Visual Studio that prints the references of all projects loaded in the solution, checks their GUIDs and prints eventual issues.

---
+++ Connect.cs
@@ -0,0 +1,263 @@
+﻿/*
+Copyright (C) 2013 Coherent Labs AD
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation the 
+rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit 
+persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions 
+of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+using System;
+using System.Collections.Generic;
+using Extensibility;
+using EnvDTE;
+using EnvDTE80;
+
+using Microsoft.VisualStudio.VCProjectEngine;
+using Microsoft.VisualStudio.CommandBars;
+
+namespace DumpReferencesAddIn
+{
+    /// <summary>The object for implementing an Add-in.</summary>
+	/// <seealso class='IDTExtensibility2' />
+    public class Connect : IDTExtensibility2, IDTCommandTarget
+	{
+		/// <summary>Implements the constructor for the Add-in object. Place your initialization code within this method.</summary>
+		public Connect()
+		{
+		}
+
+        struct RefData
+        {
+            public string Parent;
+            public string ReferencedProject;
+            public string ReferencedGUID;
+        }
+
+        private void EnumSFProjects(Project sf, List<Project> list)
+        {
+            for (var i = 1; i <= sf.ProjectItems.Count; i++)
+            {
+                var subProject = sf.ProjectItems.Item(i).SubProject;
+                if (subProject == null)
+                {
+                    continue;
+                }
+
+                if (subProject.Kind == TypeProject)
+                {
+                    list.Add(subProject);
+                }
+                else if (subProject.Kind == TypeSolutionFolder)
+                {
+                    EnumSFProjects(subProject, list);
+                }
+            }
+        }
+
+        private void DumpReferences()
+        {
+            Window window = _applicationObject.Windows.Item(Constants.vsWindowKindOutput);
+          
+            OutputWindow outputWindow = (OutputWindow)window.Object;
+            OutputWindowPane owp;
+            owp = outputWindow.OutputWindowPanes.Add("Dump References");
+
+            owp.OutputString("Initializing references dump on " + _applicationObject.Solution.FullName + "\n");
+
+            owp.OutputString("================ All project names ================ \n");
+            Dictionary<string, string> allProjectGUIDs = new Dictionary<string, string>();
+            List<Project> allProjs = new List<Project>();
+            foreach (Project proj in _applicationObject.Solution.Projects)
+            {
+                if (proj.Kind == TypeProject)
+                {
+                    allProjs.Add(proj);
+                }
+                else if(proj.Kind == TypeSolutionFolder)
+                {
+                    EnumSFProjects(proj, allProjs);                    
+                }
+            }
+
+            foreach (Project pr in allProjs)
+            {
+                VCProject vcpr = (VCProject)pr.Object;
+                owp.OutputString(vcpr.Name + "\n");
+                allProjectGUIDs.Add(vcpr.Name, vcpr.ProjectGUID);
+            }
+
+            HashSet<string> uniqueReferences = new HashSet<string>();
+
+            List<RefData> allReferences = new List<RefData>();
+            owp.OutputString("================ Full list of expanded dependencies ================ \n");
+            foreach (Project proj in allProjs)
+            {
+                owp.OutputString("--- " + proj.Name + " ---\n");
+                if (proj.Properties != null)
+                {
+                    foreach (Property prop in proj.Properties)
+                    {
+                        if (prop.Name == "VCReferences")
+                        {
+                            VCReferences references = (VCReferences)prop.Object;
+                            foreach (VCReference reference in references)
+                            {
+                                owp.OutputString(reference.Name + "\n");
+                                uniqueReferences.Add(reference.Name);
+                                RefData item = new RefData();
+                                item.Parent = proj.Name;
+                                item.ReferencedProject = reference.Name;
+
+                                VCProjectReference projRef = reference as VCProjectReference;
+                                if (projRef != null)
+                                {
+                                    item.ReferencedGUID = projRef.ReferencedProjectIdentifier;
+                                }
+                                allReferences.Add(item);
+                            }
+                        }
+                    }
+                }
+            }
+
+            owp.OutputString("================ List of expanded unique dependencies ================ \n");
+            foreach (var reference in uniqueReferences)
+            {
+                owp.OutputString(reference + "\n");
+            }
+
+            owp.OutputString("================ Possible issues ================ \n");
+            foreach (RefData reference in allReferences)
+            {
+                if (reference.ReferencedGUID != "")
+                {
+                    string projectGUIDInSolution;
+                    if (allProjectGUIDs.TryGetValue(reference.ReferencedProject, out projectGUIDInSolution))
+                    {
+                        if (projectGUIDInSolution != reference.ReferencedGUID)
+                        {
+                            owp.OutputString("Reference " + reference.ReferencedProject + " referenced by " + reference.Parent + " has a different GUID than the loaded in the solution! \n");
+                            owp.OutputString(reference.ReferencedProject + " in solution: " + projectGUIDInSolution + " in reference: " + reference.ReferencedGUID + "\n");
+                        }
+                    }
+                    else
+                    {
+                        owp.OutputString("Reference " + reference.ReferencedProject + " referenced by " + reference.Parent + " is MISSING FROM THE SOLUTION! \n");
+                    }
+                }
+                else
+                {
+                    owp.OutputString("Reference " + reference.ReferencedProject + " referenced by " + reference.Parent + " has an UNKNOWN GUID! \n");
+                }
+            }
+        }
+                
+		/// <summary>Implements the OnConnection method of the IDTExtensibility2 interface. Receives notification that the Add-in is being loaded.</summary>
+		/// <param term='application'>Root object of the host application.</param>
+		/// <param term='connectMode'>Describes how the Add-in is being loaded.</param>
+		/// <param term='addInInst'>Object representing this Add-in.</param>
+		/// <seealso class='IDTExtensibility2' />
+		public void OnConnection(object application, ext_ConnectMode connectMode, object addInInst, ref Array custom)
+		{
+			_applicationObject = (DTE2)application;
+			_addInInstance = (AddIn)addInInst;
+
+            Commands2 commands = (Commands2)_applicationObject.Commands;
+
+            try
+            {
+                _command = _applicationObject.Commands.Item(_addInInstance.ProgID + "." + COMMAND_NAME, -1);
+            }
+            catch
+            {
+            }
+            // Add the command if it does not exist
+            if (_command == null)
+            {
+                object[] contextUIGuids = new object[] { };
+                _command = commands.AddNamedCommand(_addInInstance, COMMAND_NAME, "Dump references", "Dump the references of all projects", true, 59, ref contextUIGuids,
+                                                (int)(vsCommandStatus.vsCommandStatusSupported | vsCommandStatus.vsCommandStatusEnabled));
+
+                CommandBar standardToolBar = ((CommandBars)_applicationObject.CommandBars)["Solution"];
+                CommandBarButton dumpRefsBtn = (CommandBarButton)_command.AddControl(standardToolBar, standardToolBar.Controls.Count);
+
+                // Change some button properties
+                dumpRefsBtn.Caption = "Dump References";
+                dumpRefsBtn.Style = MsoButtonStyle.msoButtonIcon;
+                dumpRefsBtn.BeginGroup = true;
+                dumpRefsBtn.Visible = true;
+            }
+		}
+
+        public void QueryStatus(string commandName, vsCommandStatusTextWanted neededText, ref vsCommandStatus status, ref object commandText)
+        {
+            if (neededText == vsCommandStatusTextWanted.vsCommandStatusTextWantedNone)
+            {
+                if (commandName == _addInInstance.ProgID + "." + COMMAND_NAME)
+                {
+                    status = (vsCommandStatus)vsCommandStatus.vsCommandStatusSupported | vsCommandStatus.vsCommandStatusEnabled;
+                    return;
+                }
+            }
+        }
+
+        public void Exec(string commandName, vsCommandExecOption executeOption,
+                 ref object varIn, ref object varOut, ref bool handled)
+        {
+            handled = false;
+            if (executeOption == vsCommandExecOption.vsCommandExecOptionDoDefault)
+            {
+                if (commandName == _addInInstance.ProgID + "." + COMMAND_NAME)
+                {
+                    DumpReferences();
+                    handled = true;
+                    return;
+                }
+            }
+        }
+
+		/// <summary>Implements the OnDisconnection method of the IDTExtensibility2 interface. Receives notification that the Add-in is being unloaded.</summary>
+		/// <param term='disconnectMode'>Describes how the Add-in is being unloaded.</param>
+		/// <param term='custom'>Array of parameters that are host application specific.</param>
+		/// <seealso class='IDTExtensibility2' />
+        public void OnDisconnection(ext_DisconnectMode disconnectMode, ref Array custom)
+        { }
+
+		/// <summary>Implements the OnAddInsUpdate method of the IDTExtensibility2 interface. Receives notification when the collection of Add-ins has changed.</summary>
+		/// <param term='custom'>Array of parameters that are host application specific.</param>
+		/// <seealso class='IDTExtensibility2' />		
+		public void OnAddInsUpdate(ref Array custom)
+		{
+		}
+
+		/// <summary>Implements the OnStartupComplete method of the IDTExtensibility2 interface. Receives notification that the host application has completed loading.</summary>
+		/// <param term='custom'>Array of parameters that are host application specific.</param>
+		/// <seealso class='IDTExtensibility2' />
+		public void OnStartupComplete(ref Array custom)
+		{
+		}
+
+		/// <summary>Implements the OnBeginShutdown method of the IDTExtensibility2 interface. Receives notification that the host application is being unloaded.</summary>
+		/// <param term='custom'>Array of parameters that are host application specific.</param>
+		/// <seealso class='IDTExtensibility2' />
+		public void OnBeginShutdown(ref Array custom)
+		{
+		}
+		
+		private DTE2 _applicationObject;
+		private AddIn _addInInstance;
+        private Command _command;
+        private string COMMAND_NAME = "DumpReference";
+
+        const string TypeProject = "{8BC9CEB8-8B4A-11D0-8D11-00A0C91BC942}";
+        const string TypeSolutionFolder = "{66A26720-8FB5-11D2-AA7E-00C04F688DDE}";
+	}
+}
--- README.md
+++ README.md
@@ -1,4 +1,69 @@
-DumpReferencesAddIn
-===================
+DumpReferences Add In
+=====================
 
-DumpReferences is an add-in for Visual Studio that prints the references of all projects loaded in the solution, checks their GUIDs and prints eventual issues.
+DumpReferences is an add-in for Visual Studio that prints the references of all projects loaded in the solution, checks
+their GUIDs and prints eventual issues. It is intended for C/C++ solutions.
+
+Purpose
+=======
+
+The add-in was created in order to alleviate problems related to two quirks in Visual Studio and msbuild:
+
+1. If you build a Solution through msbuild and project A in it depends on another project B that is NOT in that solution, project B will be 
+compiled and linked in it's default configuration. Usually this is the Debug configuration. This might lead to 
+some Debug libraries being linked in a Release project or some other config inconsistency.
+
+2. If project A in a solution references project B, also in the solution, then Visual Studio fails any compilation 
+attempt with the cryptic "The project file ' ' has been renamed or is no longer in the solution." giving no actual info 
+on what's wrong. There is a bug on msdn about this: http://connect.microsoft.com/VisualStudio/feedback/details/635201/project-file-has-been-renamed-or-is-no-longer-in-the-solution.
+It's unclear if and when it'll be fixed and most solutions suggested by users involve removing all projects, adding them one-by-one 
+and testing which one fails. This is unfeasible in large solutions.
+
+How it works
+=============
+
+The add-in is fairly simple - it walks all projects in the solution and their references and prints:
+ - all projects currently in the solution
+ - all references that each project has
+ - all unique references in the solution
+ - possible issues: referenced projects missing from the solution; projects available in the solution but referenced by other projects with another GUID
+
+To dump the diagnostic just right-click on your solution in the Solution Explorer and click "Dump References".
+The results will be available in the Output window in the Dump References pane.
+ 
+Build
+======
+
+Just build the solution and place the output DLL and DumpReferencesAddIn.AddIn in your VS add-in folder.
+
+Testing
+=======
+
+If you want to test the add-in you could follow the instructions on how to do it available on MSDN, or just:
+ - Rename DumpReferencesAddIn.AddIn to DumpReferencesAddInTesting.AddIn or something and make it point to the output Debug DLL of the add-in
+ - Run the debugger on VS - that is in the project config under "Debug"->"Start Action" select "Start external program". 
+ Point to "(VSInstallFolder)devenv.exe /resetaddin DumpReferencesAddIn.Connect" where "(VSInstallFolder)" is obviously the folder 
+ where your VS IDE is installed.
+
+Testing Done
+============
+
+The add-in has been successfully tested on MSVS 2012. It should also work on MSVS 2010 and 2008.
+
+License
+========
+Copyright (C) 2013 Coherent Labs AD
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated 
+documentation files (the "Software"), to deal in the Software without restriction, including without limitation the 
+rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit 
+persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions 
+of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED 
+TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL 
+THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
+TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
